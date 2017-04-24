87ad2cc10435f6a12b822e3e22efc41c3cd04b22
xubinux <xu.binux@gmail.com>
2017-04-24 21:03:41 +0800

    修改Bug

--- xbin-store-service-admin/src/main/java/cn/binux/XbinStoreServiceAdminApplication.java
+++ xbin-store-service-admin/src/main/java/cn/binux/XbinStoreServiceAdminApplication.java
@@ -1,5 +1,6 @@
 package cn.binux;
 
+import com.alibaba.dubbo.container.Main;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,6 @@ public class XbinStoreServiceAdminApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XbinStoreServiceAdminApplication.class, args);
-
+		Main.main(args);
 	}
 }
